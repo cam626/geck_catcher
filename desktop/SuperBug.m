2469849e5565539e7b0fa701bed5c20b55a91074
Cameron Scott <camscott678@gmail.com>
2020-07-10 15:37:07 -0400

    First game logic (player and bug movement)

--- desktop/SuperBug.m
+++ desktop/SuperBug.m
@@ -8,5 +8,57 @@ classdef SuperBug
         P
         V
         A
+        worldWidth
+        worldHeight
+        worldDepth
+    end
+    
+    methods
+        function obj = SuperBug(worldSize, maxSpeed)
+            obj.worldWidth = worldSize(1);
+            obj.worldHeight = worldSize(2);
+            obj.worldDepth = worldSize(3);
+            
+            obj.P = [obj.worldWidth*rand(1), obj.worldHeight*rand(1), obj.worldDepth*rand(1)];
+            obj.V = -1 * maxSpeed / 2 + maxSpeed*rand(1, 3);
+        end
+        
+        function obj = update(obj)
+            if obj.P(1) + obj.V(1) > obj.worldWidth
+                obj.P(1) = obj.worldWidth;
+                obj.V(1) = obj.V(1) * -1;
+            end
+            
+            if obj.P(1) + obj.V(1) < 0
+                obj.P(1) = 0;
+                obj.V(1) = obj.V(1) * -1;
+            end
+            
+            if obj.P(2) + obj.V(2) > obj.worldHeight
+                obj.P(2) = obj.worldHeight;
+                obj.V(2) = obj.V(2) * -1;
+            end
+            
+            if obj.P(2) + obj.V(2) < 0
+                obj.P(2) = 0;
+                obj.V(2) = obj.V(2) * -1;
+            end
+            
+            if obj.P(3) + obj.V(3) > obj.worldDepth
+                obj.P(3) = obj.worldDepth;
+                obj.V(3) = obj.V(3) * -1;
+            end
+            
+            if obj.P(3) + obj.V(3) < 0
+                obj.P(3) = 0;
+                obj.V(3) = obj.V(3) * -1;
+            end
+            
+            obj.P = obj.P + obj.V;
+        end
+        
+        function pos = getPosition(obj)
+            pos = obj.P;
+        end
     end
 end
